4813f2eb13e269159ca9fd001439db6ee03942a4
Edsey omenda <omendajames0317@gmail.com>
2022-06-28 17:43:04 +0300

    commit tree a1d4027c8174f65577d2bf42ca3558147cd4234b

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+
+
+class Cat {
+    constructor(Sasha, female) {
+        this.name = Sasha;
+        this.sex = female;
+    }
+    speak() {
+        return `${this.name} says meow!`;
+    }
+    // speak()
+};
+
+
+class Dog {
+    constructor(Rufio, male) {
+        this.name = Rufio;
+        this.sex = male;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+
+}
+
+class Bird {
+    constructor(Pablo, male) {
+        this.name = Pablo;
+        this.sex = male;
+    }
+    speak() {
+        if (this.sex == 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else if(this.sex === 'female'){
+            return `${this.name} says squawk!`
+        }
+   
+         }}
+
+        //  class bird2 {
+        //     constructor(Mable, female) {
+        //         this.name = Mable;
+        //         this.sex = female;
+        //     }
+        //     speak() {
+        //             return `${this.name} says squawk!`
+           
+        //          }
+                
+        //         }
+
+     
